05e3fb568654ee728e94139f64332220301c339d
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2024-04-18 14:27:45 +0300

    added new table 'Descriptions'

--- routes.py
+++ routes.py
@@ -91,8 +91,8 @@ def all_plants():
 
     return render_template("all_plants.html", count=len(plants), plants=plants, rights=rights)
 
-@app.route("/new_plant_page")
-def new_plant_page():
+@app.route("/new_plant")
+def new_plant():
     categories = db.session.execute(text("SELECT * FROM Categories"))    
     return render_template("new_plant.html", categories=categories)
 
@@ -102,10 +102,22 @@ def save_plant():
     plant_category = request.form["category"]
     plant_price = int(request.form["price"])
     plant_description = request.form["description"]
+    username = session["username"]
+
+    result = db.session.execute(text("SELECT id FROM Accounts WHERE username=:username"), {"username" :username})
+    username_id = int(result.fetchone()[0])
+    
+    
     sql = text("INSERT INTO Plants (name, price, category_id ) VALUES (:name, :price, :category_id)")
     db.session.execute(sql, {"name": plant_name, "price": plant_price, "category_id": plant_category})
     db.session.commit()
+    
+    result = db.session.execute(text("SELECT id FROM Plants WHERE name=:name"), {"name" :plant_name})
+    plant_id = int(result.fetchone()[0])
 
-    #print(plant_name, plant_category, plant_price, plant_description)
+    sql = text("INSERT INTO Descriptions (content, plant_id, username_id) VALUES (:content, :plant_id, :username_id)")
+    db.session.execute(sql, {"content": plant_description, "plant_id": plant_id, "username_id": username_id} )
+    db.session.commit()
+
     return redirect("/all-plants")
 
--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS Messages CASCADE;
 DROP TABLE IF EXISTS Plants CASCADE;
 DROP TABLE IF EXISTS Accounts CASCADE;
 DROP TABLE IF EXISTS Categories CASCADE;
+DROP TABLE IF EXISTS Descriptions CASCADE;
 
 CREATE TABLE Messages (
     id SERIAL PRIMARY KEY,
@@ -26,6 +27,13 @@ CREATE TABLE Plants (
 CREATE TABLE Categories (
     id SERIAL PRIMARY KEY, 
     name TEXT
+);
+
+CREATE TABLE Descriptions (
+    id SERIAL PRIMARY KEY, 
+    content TEXT,
+    plant_id INTEGER,
+    username_id INTEGER
 );
 
 INSERT INTO Categories (name) VALUES ('Nahkealehtiset kasvit');
